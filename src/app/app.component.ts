54e21d4798afa23af5958dc94ae1d19af893c36f
dnyuhm <dylanmeuriot@gmail.com>
2023-04-11 14:53:54 +0200

    Commit 1 : Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'crm';
+  public title!: string;
+
+  constructor() {
+    this.title = 'crm';
+    console.log(this.title);
+  }
 }
