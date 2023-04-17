aec5ac94403822418d0af6b4ee403e2d0f3a4bf9
dnyuhm <dylanmeuriot@gmail.com>
2023-04-17 11:51:40 +0200

    Commit 19 : Enums, interfaces & models

---
+++ src/app/orders/models/order.ts
@@ -0,0 +1,13 @@
+import { StateOrder } from '../enums/state-order';
+import { OrderI } from '../interfaces/order-i';
+
+export class Order implements OrderI {
+  tjmHt = 400;
+  nbJours = 3;
+  tva = 20;
+  state = StateOrder.OPTION;
+  typePresta!: string;
+  client!: string;
+  comment!: string;
+  id!: number;
+}
