4fc2e2bf4b37d165ad214ff56c28ef1db8764370
dnyuhm <dylanmeuriot@gmail.com>
2023-04-18 11:05:10 +0200

    Commit 24 : Exercice “version” sur les services & observables

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { VersionService } from '../../services/version.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  public numVersion!: number;
+  constructor(private versionService: VersionService) {
+    this.versionService.version$.subscribe((data) => {
+      this.numVersion = data;
+    });
+  }
 }
